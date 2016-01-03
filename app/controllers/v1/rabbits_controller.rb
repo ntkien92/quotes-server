0aa323e86c9029e033039aa571607ec45076f515
NTKIEN92 <ntkien92@gmail.com>
2016-01-03 18:54:06 +0700

    [UPDATE] - Rename items
     On branch master
     Changes to be committed:
    	modified:   app/controllers/v1/rabbits_controller.rb

--- app/controllers/v1/rabbits_controller.rb
+++ app/controllers/v1/rabbits_controller.rb
@@ -1,6 +1,6 @@
 class V1::RabbitsController < ApplicationController
   def index
     @rabbits = Rabbit.all.page(params[:page])
-    render json: @rabbits
+    render json: @rabbits, root: 'items'
   end
 end
