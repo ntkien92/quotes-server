845c3d2943dcd5d3fa1f0d38fd513c6cf0ecb2ae
nguyen trung kien <ntkien92@gmail.com>
2016-01-07 20:38:07 +0700

    [ADD] - Add total page

--- app/controllers/v1/inspirations_controller.rb
+++ app/controllers/v1/inspirations_controller.rb
@@ -1,10 +1,7 @@
 class V1::InspirationsController < ApplicationController
   def index
     items = Inspiration.all.order("id desc").page(params[:page])
-    page = items.total_pages
-    render json: {
-      page: page,
-      items: items
-      }
+    total_page = items.total_pages
+    render json: items, root: 'items', meta: { total: items.total_pages }
   end
 end
--- app/controllers/v1/rabbits_controller.rb
+++ app/controllers/v1/rabbits_controller.rb
@@ -1,10 +1,7 @@
 class V1::RabbitsController < ApplicationController
   def index
     items = Rabbit.all.order("id desc").page(params[:page])
-    page = items.total_pages
-    render json: {
-      page: page,
-      items: items
-      }
+    total_page = items.total_pages
+    render json: items, root: 'items', meta: { total: items.total_pages }
   end
 end
