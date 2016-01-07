c8281a04bc0ab13645b1eb05c26336953e61df0b
nguyen trung kien <ntkien92@gmail.com>
2016-01-07 22:21:42 +0700

    [UPDATE] - Add item page

--- app/controllers/v1/inspirations_controller.rb
+++ app/controllers/v1/inspirations_controller.rb
@@ -1,7 +1,6 @@
 class V1::InspirationsController < ApplicationController
   def index
     items = Inspiration.all.order("id desc").page(params[:page])
-    total_page = items.total_pages
-    render json: items, root: 'items', meta: { total: items.total_pages }
+    render json: items, root: 'items', meta: items.total_pages
   end
 end
--- app/controllers/v1/rabbits_controller.rb
+++ app/controllers/v1/rabbits_controller.rb
@@ -1,7 +1,6 @@
 class V1::RabbitsController < ApplicationController
   def index
     items = Rabbit.all.order("id desc").page(params[:page])
-    total_page = items.total_pages
-    render json: items, root: 'items', meta: { total: items.total_pages }
+    render json: items, root: 'items', meta: items.total_pages
   end
 end
