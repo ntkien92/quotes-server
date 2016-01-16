83aa24c55a477bdf6d66e8a88642e5089cb7abbe
nguyen trung kien <ntkien92@gmail.com>
2016-01-16 18:30:09 +0700

    [UPDATE] - Not nul database

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,6 @@ class Quote < ActiveRecord::Base
   scope :get_items, ->(index) {
     where('id > ?', index)
   }
+  validates :image, presence: true
+  validates :type_quote, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   api_version(:module => "v1", :path => {value: 'v1'}) do
-    resources :posts, only: [:index, :show]
-    resources :rabbits, only: [:index]
-    resources :inspirations, only: [:index]
     resources :quotes, only: [:index]
   end
-
-  resources :rabbits
 end
